df61109129832e40449de766c4251c6215510d5f
Björn Kötter <git@bk.netburn.de>
2026-05-17 11:55:02 +0200

    fix: improve error handling in openFile function and update header file path

--- MultiFile/src/file.c
+++ MultiFile/src/file.c
@@ -2,17 +2,19 @@
 // Created by Björn Kötter on 16.05.26.
 //
 
-#include "../file.h"
+#include "../src/file.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/fcntl.h>
 
 int openFile(const char* fileName, const char* mode)
 {
-    int fd = open(fileName, O_RDONLY);
+    const int fd = open(fileName, O_RDONLY);
     if (fd == -1)
     {
-        perror("open");
+        perror("Error opening file");
+        exit(1);
     }
     return fd;
 }
